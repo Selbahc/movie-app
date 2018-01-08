2ae951453b4962c2e5724e02370c92d7637c7679
Selbahc <selbahc@users.noreply.github.comm>
2018-01-08 18:57:03 +0100

    added infinite scroll to popular, fixed authentication issue

--- src/components/fetchPopular.js
+++ src/components/fetchPopular.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import apiConfig from '../../config-api';
 
@@ -10,24 +11,35 @@ class FetchPopular extends Component {
     super(props)
     this.state = {
       popularList: [],
-      fetching: true
+      fetching: true,
+      pageToFetch: 1
     } 
   }
-
-  componentDidMount() {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiConfig.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
-      .then(response => response.json())
-      .then(data => this.setState({ popularList: data.results, fetching: false }));
-  }
   
+  fetchPopular() {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiConfig.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=${this.state.pageToFetch}`)
+    .then(response => response.json())
+    .then(data => this.setState(prevState => {
+      return { 
+        pageToFetch: prevState.pageToFetch += 1,
+        popularList: [...this.state.popularList, ...data.results], 
+        fetching: false
+      }
+    }))
+    .catch(err => {
+      throw err 
+    });
+  }
+
   render() {
     return (
-      <div>
-        {this.state.fetching
-          ? <CircularProgress size={80} thickness={5} style={{display: 'block', margin: '2em auto'}} /> 
-          : <MovieDisplay openSnackbar={this.props.openSnackbar} movies={this.state.popularList} />
-        }
-      </div>
+      <InfiniteScroll
+        loadMore={() => this.fetchPopular()}
+        hasMore={true}
+        loader={<CircularProgress style={{display: "block", margin: "0 auto"}}/>}
+      >
+        <MovieDisplay openSnackbar={this.props.openSnackbar} movies={this.state.popularList} />
+      </InfiniteScroll>
     );
   }
 }
--- src/utils/tokenHeaders.js
+++
@@ -1,4 +0,0 @@
-const tokenHeader = new Headers();
-tokenHeader.set('token', sessionStorage.getItem('token'));
-
-export default tokenHeader;
